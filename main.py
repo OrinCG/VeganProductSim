c6fbfacf8f32e9c07925ca2e27e27adedacb63ce
ocg1g20 <ocg1g20@soton.ac.uk>
2022-07-25 15:22:29 +0100

    Got graphs to update on a press

--- main.py
+++ main.py
@@ -13,57 +13,68 @@ from matplotlib.backends.backend_tkagg import (
 from tkinter import ttk
 
 
-
 class AnimalGameGui:
-     def __init__(self, no_agents):
+    def __init__(self, no_agents):
         self.no_agents = no_agents
         self.product1_sold = 200
         self.product2_sold = 39
         self.product3_sold = 199
 
         self.window = tk.Tk()
+        # self.middle = tk.Frame(self.window)
+        # self.bottom = tk.Frame(self.window)
+        # self.middle.pack()
+        # self.bottom.pack()
+        self.graph  = None
         self.show_bar()
 
-
-     def start_sim(self):
-
+    def start_sim(self):
         self.window.mainloop()
 
-     def show_bar(self):
-         data = {
-             'P1':self.product1_sold,
-             'P2': self.product2_sold,
-             'P3': self.product3_sold
-         }
+    def show_bar(self):
+        data = {
+            'P1': self.product1_sold,
+            'P2': self.product2_sold,
+            'P3': self.product3_sold
+        }
 
-         figure = Figure(figsize=(6,4), dpi=100)
-         # create FigureCanvasTkAgg object
-         figure_canvas = FigureCanvasTkAgg(figure, self.window)
+        figure = Figure(figsize=(6, 4), dpi=100)
+        # create FigureCanvasTkAgg object
+        figure_canvas = FigureCanvasTkAgg(figure, self.window)
 
-         # create the toolbar
-         NavigationToolbar2Tk(figure_canvas, self.window)
+        # # create the toolbar
+        # NavigationToolbar2Tk(figure_canvas, self.window)
 
-         # create axes
-         axes = figure.add_subplot()
+        # create axes
+        axes = figure.add_subplot()
 
-         # create the barchart
-         rects = axes.bar(data.keys(), data.values())
-         axes.set_title('Products')
-         axes.set_ylabel('Num Sold')
+        # create the barchart
+        rects = axes.bar(data.keys(), data.values())
+        axes.set_title('Products')
+        axes.set_ylabel('Num Sold')
 
-      #   self.window.__setitem__()
-         figure_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        #   self.window.__setitem__()
+        self.graph = figure_canvas.get_tk_widget()
+        self.graph.pack()
 
-         test_button = tk.Button(self.window,text = "Increase bar 1", command=     lambda: rects[1].set_height(rects[1].get_height()+30)
-)
 
-         test_button.pack()
+
+        def increaseHeight():
+            rects[1].set_height(rects[1].get_height() + 30)
+            print(rects[1].get_height())
+            figure_canvas.draw()
+
+        test_button = tk.Button(self.window, text="Increase bar 1",
+                                command=lambda: increaseHeight()
+                                )
+        test_button.grid_anchor("s")
+        test_button.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     gui = AnimalGameGui(10)
-    print("hi")
+    #  print("hi")
     gui.start_sim()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
