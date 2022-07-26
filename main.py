fa399c8f8fe9b8fbd0ecb4485a15bf8706779c48
ocg1g20 <ocg1g20@soton.ac.uk>
2022-07-26 12:25:13 +0100

    Started on bar chart animations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pymoo
 import tkinter as tk
 import matplotlib
+import matplotlib.animation as animation
 import time
 
 matplotlib.use('TkAgg')
@@ -25,7 +26,7 @@ class AnimalGameGui:
         # self.bottom = tk.Frame(self.window)
         # self.middle.pack()
         # self.bottom.pack()
-        self.graph  = None
+        self.graph = None
         self.show_bar()
 
     def start_sim(self):
@@ -41,13 +42,8 @@ class AnimalGameGui:
         figure = Figure(figsize=(6, 4), dpi=100)
         # create FigureCanvasTkAgg object
         figure_canvas = FigureCanvasTkAgg(figure, self.window)
-
-        # # create the toolbar
-        # NavigationToolbar2Tk(figure_canvas, self.window)
-
         # create axes
         axes = figure.add_subplot()
-
         # create the barchart
         rects = axes.bar(data.keys(), data.values())
         axes.set_title('Products')
@@ -57,12 +53,15 @@ class AnimalGameGui:
         self.graph = figure_canvas.get_tk_widget()
         self.graph.pack()
 
+        # Update the graph as the values change
+        def graph_ani(frame):
+            # rects[1].set_height(rects[1].get_height() + 30)
+            # print(rects[1].get_height())
+            # figure_canvas.draw()
+            new_vals = map(lambda x: x + 10, data.values())
+            rects = axes.bar(data.keys, new_vals)
 
-
-        def increaseHeight():
-            rects[1].set_height(rects[1].get_height() + 30)
-            print(rects[1].get_height())
-            figure_canvas.draw()
+        update_graph = animation.FuncAnimation(figure, graph_ani, 500)
 
         test_button = tk.Button(self.window, text="Increase bar 1",
                                 command=lambda: increaseHeight()
