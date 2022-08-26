94d77c0c14c596621a9d7319e0012a6200e9798c
ocg1g20 <ocg1g20@soton.ac.uk>
2022-08-26 16:40:23 +0100

    Added run for the model, needs to be connected up to the view.
    Also finished agents step which needs testing.

--- model.py
+++ model.py
@@ -1,49 +1,55 @@
 import pymoo
 import numpy as np
 import random
-from mesa import Model as mesModel
-from mesa import Agent as mesAgent
-from mesa import time as mesTime
+import mesa
 
 import pymoo.core.problem as pyProb
 
 
+def get_avg (item_list, attribute):
+    return sum(list(map(lambda p: getattr(p, attribute), item_list))) / len(item_list)
+
 def normalise(products):
-    avg_cost = 0
-    avg_cost = sum(list(map(lambda p: getattr(p,"cost"), products)))/len(products)
-    new_products = []
+    avg_cost = get_avg(products,"cost")
     for product in products:
         product.cost = product.cost / avg_cost
 
-class Model(mesModel):
 
-    def __init__(self, no_agents):
+class Model(mesa.Model):
+
+    def __init__(self, no_agents, opinion_change_rate):
         self.no_agents = no_agents
-        self.schedule = mesTime.RandomActivation(self)
+        self.opinion_change_rate = opinion_change_rate
+        self.schedule = mesa.time.RandomActivation(self)
         for i in range(self.no_agents):
             agent = Agent(i, self)
             self.schedule.add(agent)
 
-        #Asign each agent friends who influence them
+        # Asign each agent friends who influence them
         for a in self.schedule.agents:
             i = random.randint(0, self.no_agents - 1)
             i2 = random.randint(0, self.no_agents - 1)
             a.__getattribute__("friends").extend([i, i2])
             print(a.__getattribute__("friends"))
 
-        self.product1_sold = 0
-        self.product2_sold = 0
-        self.product3_sold = 0
-        self.products = [Product("Organic Pea Meat",10.60,20), Product("Fake chicken",7,50), Product("Soy meat",2.50,80)]
+        self.products = [Product("Organic Pea Meat", 10.60, 20), Product("Fake chicken", 7, 50),
+                         Product("Soy meat", 2.50, 80)]
         normalise(self.products)
         print(self.products)
-
 
     def step(self):
         self.schedule.step()
 
 
-class Agent(mesAgent):
+    def run(self):
+        for i in range(10):
+            self.step()
+            #Update view
+
+
+
+
+class Agent(mesa.Agent):
 
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
@@ -51,13 +57,42 @@ class Agent(mesAgent):
         self.friends = []
         self.costScore = 1 - self.envScore
 
+    def step(self) -> None:
+        self.exchange_opinions()
+
+        product_scores = list(map(self.calcScore(), self.model.__getattribute__("products")))
+        max(product_scores).sold += 1
+
+    #Alter this agents scores to be a little closer to a friends
+    def exchange_opinions(self):
+        exchange_f = self.friends[random.randint(0, len(self.friends))]
+        ex_f_env = exchange_f.__getattribute__("envScore")
+        ex_f_cost = exchange_f.__getattribute__("costScore")
+        op_change_rate = self.model.__getattribute__("opinion_change_rate")
+        env_change = abs(ex_f_env - self.envScore) / op_change_rate
+        cost_change = abs(ex_f_cost - self.costScore) / op_change_rate
+        if self.costScore < ex_f_cost:
+            self.costScore += cost_change
+        else:
+            self.costScore -= cost_change
+        if self.envScore < ex_f_env:
+            self.envScore += env_change
+        else:
+            self.envScore -= env_change
+
+    def calcScore(self):
+        products = self.model.__getattribute__("products")
+        return lambda prod: ((100 - prod.env_impact) * self.envScore) / 100 + (
+                prod.cost * self.costScore) #Max env impact is 100 , should change to not be hard coded
+
+
 class Product:
 
-    def __init__(self,name,cost,env_impact):
+    def __init__(self, name, cost, env_impact):
         self.name = name
         self.cost = cost
         self.env_impact = env_impact
-
+        self.sold = 0
 
     def __str__(self):
         return "Product: " + self.name + " " + str(self.cost) + " " + str(self.env_impact)
@@ -67,6 +102,7 @@ class Product:
 
     def __repr__(self):
         return "Product: " + self.name + " " + str(self.cost) + " " + str(self.env_impact)
+
 
 # def step(self):
 
